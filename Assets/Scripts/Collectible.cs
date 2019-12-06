5a272abdc9018f552838e0051a10b59e4eb97c45
Antho <transkuja@hotmail.fr>
2019-12-06 23:57:54 +0100

    Collectible progress

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+    static int count = 0;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        count++;
+        gameObject.SetActive(false);
+    }
+}
