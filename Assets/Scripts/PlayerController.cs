5bddbed2f92e7aac0b37817a23ee3a3c0afa8ded
Antho <transkuja@hotmail.fr>
2019-12-07 11:07:21 +0100

    Init player controller movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    Rigidbody rb;
+    [SerializeField] float accelerationFactor;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float pietinementThreshold;
 
-	void Start () {
-		
-	}
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
 	
 	void Update () {
         Jump();
-	}
+        MovePlayer();
+
+    }
 
     void Jump()
     {
 
+    }
+
+    void MovePlayer()
+    {
+        transform.LookAt(transform.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")));
+
+        if (Input.GetAxisRaw("Horizontal") > 0.1f ||
+                Input.GetAxisRaw("Horizontal") < -0.1f ||
+                Input.GetAxisRaw("Vertical") > 0.1f ||
+                Input.GetAxisRaw("Vertical") < -0.1f)
+        {
+            if (rb.velocity.magnitude < pietinementThreshold)
+                rb.AddForce(transform.forward * accelerationFactor);
+
+            else
+                rb.velocity = transform.forward * maxSpeed;
+
+            //Debug.Log(rb.velocity.magnitude);
+            //if (rb.velocity.magnitude < pietinementThreshold)
+            //{
+            //    Debug.Log("lol");
+            //    rb.AddForce(transform.forward * accelerationFactor);
+            //}
+            //else
+            //{
+            //    rb.velocity = transform.forward * maxSpeed;
+            //}
+        }
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
     }
 }
