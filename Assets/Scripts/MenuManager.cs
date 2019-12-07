2ab7584aa37696955cf9928b329e98068f1d22f7
Leios <leiosy29@gmail.com>
2019-12-07 14:07:21 +0100

    Menu (Scene, UI, ect...)

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour {
+
+    [SerializeField]
+    private Button buttonPlay;
+
+    [SerializeField]
+    private Button buttonCredit;
+
+    [SerializeField]
+    private Button buttonQuit;
+
+    [SerializeField]
+    private GameObject goCredit;
+
+    private void Start()
+    {
+        buttonPlay.onClick.AddListener(Play);
+        buttonCredit.onClick.AddListener(Credit);
+        buttonQuit.onClick.AddListener(Quitter);
+    }
+
+    public void Play()
+    {
+       SceneManager.LoadScene(0);
+    }
+
+    public void Credit()
+    {
+        goCredit.SetActive(true);
+    }
+
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+}
