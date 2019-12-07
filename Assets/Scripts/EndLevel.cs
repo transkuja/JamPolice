a42227359f7a973504c34257a8f3a202c4687092
Leios <leiosy29@gmail.com>
2019-12-07 14:35:29 +0100

    J'ai tout comit jpp !

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject Recap;
+
+    [SerializeField]
+    private GameObject Player;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>() != null)
+        { 
+            other.GetComponent<PlayerController>().controlsLocked = true;
+
+            other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            other.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+            Recap.SetActive(true);
+        }
+    }
+}
