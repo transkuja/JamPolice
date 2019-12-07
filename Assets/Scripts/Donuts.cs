5f89d96fae7681671a24ec6ca39a3c2208604aee
Leios <leiosy29@gmail.com>
2019-12-07 13:16:47 +0100

    Player Prefab + Donuts

---
+++ Assets/Scripts/Donuts.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Donuts : MonoBehaviour {
+    static int donutscount = 0;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        transform.Rotate(Vector3.up * 2.0f);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        donutscount++;
+        gameObject.SetActive(false);
+    }
+}
