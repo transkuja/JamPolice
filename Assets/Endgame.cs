fa32552511676e1159de2d85b68ed6ad5041f1d3
Antho <transkuja@hotmail.fr>
2019-12-08 06:16:13 +0100

    Endgame bitchiz!

--- Assets/Endgame.cs
+++ Assets/Endgame.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Endgame : MonoBehaviour {
-
-    public GameObject endgameMenu;
+    public List<GameObject> toSpawn;
+    public GameObject felicitations;
+    public List<Transform> spawnPoints;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,9 +15,41 @@ public class Endgame : MonoBehaviour {
         {
             PlayerController playerController = FindObjectOfType<PlayerController>();
             playerController.controlsLocked = true;
-            endgameMenu.SetActive(true);
             GetComponent<Collider>().enabled = false;
+            felicitations.SetActive(true);
+
+            if (GameData.Donutscount == 100)
+            {
+                felicitations.GetComponentInChildren<Text>().text = "Vous avez arrete tous les bandits et reuni tous les donuts !";
+                StartCoroutine(EndgameCoroutine());
+            }
+            else
+            {
+                felicitations.GetComponentInChildren<Text>().text = "Vous avez arrete tous les bandits !";
+                Invoke("BackToMenu", 5.0f);
+            }
         }
     }
 
+    void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    IEnumerator EndgameCoroutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+
+        while (true)
+        {
+            foreach (var spawnpoint in spawnPoints)
+            {
+                GameObject newInstance = Instantiate(toSpawn[Random.Range(0, toSpawn.Count)], spawnpoint);
+                newInstance.transform.localEulerAngles = new Vector3(Random.Range(0, 180), Random.Range(0, 180), Random.Range(0, 180));
+                Destroy(newInstance, 5.0f);
+                yield return new WaitForSeconds(0.2f);
+            }
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
 }
