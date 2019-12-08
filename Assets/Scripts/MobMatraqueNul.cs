ad3fb4bfe55b3c6a28206d67e3a166415a4c2709
Antho <transkuja@hotmail.fr>
2019-12-08 02:25:03 +0100

    Base enemy

---
+++ Assets/Scripts/MobMatraqueNul.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MobMatraqueNul : Enemy {
+    NavMeshAgent agent;
+    Animator animator;
+
+    private void Awake()
+    {
+        sensibleToJump = true;
+        sensibleToHit = true;
+        sensibleToProjectile = true;
+    }
+    Vector3 initPosition;
+
+    IEnumerator Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+        initPosition = transform.position;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(3.0f, 6.0f));
+            agent.SetDestination(initPosition + Random.insideUnitSphere * 5);
+        }
+    }
+
+    void Update()
+    {
+        if (agent != null && animator != null)
+            animator.SetFloat("velocity", agent.velocity.magnitude);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        animator.SetTrigger("giracomoeltopo");
+    }
+}
